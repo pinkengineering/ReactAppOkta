b6c82b4f2abeac174741f85328377db0b4b44500
Stephanie Miranda <smirandanieves@gmail.com>
2024-11-21 22:30:31 -0400

    updates

--- Sample-01/src/config.js
+++ Sample-01/src/config.js
@@ -6,9 +6,13 @@ export function getConfig() {
       ? process.env.REACT_APP_AUTH0_AUDIENCE
       : null;
 
+  // Add redirectUri
+  const redirectUri = process.env.REACT_APP_AUTH0_REDIRECT_URI || window.location.origin;
+
   return {
     domain: process.env.REACT_APP_AUTH0_DOMAIN,
     clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
+    redirectUri, // Include the redirectUri
     ...(audience ? { audience } : null),
   };
 }
